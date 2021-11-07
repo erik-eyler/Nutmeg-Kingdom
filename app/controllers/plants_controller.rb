018c7163719cdfe9e49f08095f08e3191d6381cd
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-11-07 18:54:42 -0500

    add_season plants controller and updated route

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
   before_action :set_plant, only: [:show, :update, :destroy]
+  before_action :authorize_request, only: [:create, :update, :destroy]
+  before_action :set_user_plant, only: [:update, :destroy]
 
   # GET /plants
   def index
@@ -10,15 +12,15 @@ class PlantsController < ApplicationController
 
   # GET /plants/1
   def show
-    render json: @plant
+    render json: @plant, include: :seasons
   end
 
   # POST /plants
   def create
     @plant = Plant.new(plant_params)
-
+    @plant.user = @current_user
     if @plant.save
-      render json: @plant, status: :created, location: @plant
+      render json: @plant, status: :created
     else
       render json: @plant.errors, status: :unprocessable_entity
     end
@@ -38,10 +40,23 @@ class PlantsController < ApplicationController
     @plant.destroy
   end
 
+  def add_season
+    @season = Season.find(params[:season_id])
+    @plant = Plant.find(params[:plant_id])
+
+    @plant.seasons << @season
+
+    render json: @plant, include: :seasons
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_plant
       @plant = Plant.find(params[:id])
+    end
+
+    def set_user_plant
+      @plant = @current_user.plants.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :seasons
+  resources :seasons, only: [:index, :show]
   resources :plants
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
-  resources :users
+  resources :users, only: :create
+  put '/seasons/:season_id/plants/:id', to: 'plants#add_season'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
