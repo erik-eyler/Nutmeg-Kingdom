1addd34d23e2a0b7b92ad6b8887096787074a111
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-11-08 08:50:23 -0500

    PlantEdit.jsx done except for planting_date and img

--- client/src/screens/PlantEdit.jsx
+++ client/src/screens/PlantEdit.jsx
@@ -1,7 +1,47 @@
-export default function PlantEdit() {
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function PlantEdit(props) {
+  const [formData, setFormData] = useState({
+    name: '',
+  });
+  const { name } = formData;
+  const { id } = useParams();
+  const { plants, handlePlantUpdate } = props;
+
+  useEffect(() => {
+    const prefillFormData = () => {
+      const plantItem = plants.find(plant => plant.id === Number(id))
+      setFormData({
+        name: plantItem.name
+      })
+    };
+    if (plants.length) {
+      prefillFormData();
+    }
+  }, [plants, id]);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setFormData({
+      name: value,
+    });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handlePlantUpdate(id, formData);
+      }}
+    >
+      <h1>Edit Plant</h1>
+      <label>
+        Name:
+        <input type='text' value={name} onChange={handleChange} />
+      </label>
+      <br />
+      <button>Submit</button>
+    </form>
+  );
 }
