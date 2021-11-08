a716993fa723e409503e862a609b04ec0fccbdbe
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-11-07 22:52:58 -0500

    Layout.jsx added ternary for user login/out and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Layout from './layouts/Layout';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useHistory } from 'react-router-dom'
 import Login from './screens/Login';
 import { loginUser, registerUser } from './services/auth';
 import { useState, useEffect } from 'react';
@@ -8,20 +8,23 @@ import Register from './screens/Register'
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
 
   const handleLogin = async (formData) => {
     const userData = await loginUser(formData);
     setCurrentUser(userData)
+    history.push('/')
   };
 
   const handleRegister = async (formData) => {
     const userData = await registerUser(formData);
     setCurrentUser(userData);
+    history.push('/')
   }
 
   return (
     <div className="App">
-      <Layout>
+      <Layout currentUser={currentUser}>
         <Switch>
           <Route path='/login'>
             <Login handleLogin={handleLogin} />
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,14 +1,30 @@
 import {Link} from 'react-router-dom'
 
 export default function Layout(props) {
+  const { children, currentUser, handleLogout } = props;
+
+
   return (
     <div>
       <header>
         <h1>Nutmeg Kingdom</h1>
-        <Link to='/login'>Login/Register</Link>
+        {currentUser ? (
+          <div>
+            <p>{currentUser.username}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <Link to='/login'>Login/Register</Link>
+        )}
         <hr />
+        {currentUser && (
+          <div>
+            <Link to='/plants'>Plants</Link>
+            <Link to='/create'>Add Plant</Link>
+          </div>
+        )}
       </header>
-      {props.children}
+      {children}
     </div>
-  )
+  );
 }
