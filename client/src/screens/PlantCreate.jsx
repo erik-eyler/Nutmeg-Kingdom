74afdd5a6ea3c78bd23c249750353725a42d286c
Erik Eyler <erikeyler@Eriks-MacBook-Air.local>
2021-11-08 08:44:10 -0500

    PlantCreate.jsx almost finished, need to add plant dates and img later

--- client/src/screens/PlantCreate.jsx
+++ client/src/screens/PlantCreate.jsx
@@ -1,7 +1,33 @@
-export default function PlantCreate() {
+import { useState } from 'react';
+
+export default function PlantCreate(props) {
+  const [formData, setFormData] = useState({
+    name: '',
+  });
+  const { name } = formData;
+  const { handlePlantCreate } = props;
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setFormData({
+      name: value,
+    });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handlePlantCreate(formData);
+      }}
+    >
+      <h1>Add Plant</h1>
+      <label>
+        Name:
+        <input type='text' value={name} onChange={handleChange} />
+      </label>
+      <br />
+      <button>Submit</button>
+    </form>
+  );
 }
